587aa18cef560b6ed3df30b651272eb9b5743015
Chenmin <cnlll@qq.com>
2019-03-01 16:04:27 +0800

    login remember ok

--- src/main/java/org/chenmin/gwt/client/pages/LoginComponent.java
+++ src/main/java/org/chenmin/gwt/client/pages/LoginComponent.java
@@ -7,12 +7,15 @@ import com.axellience.vuegwt.core.client.component.hooks.HasCreated;
 import com.axellience.vueroutergwt.client.Route;
 import com.axellience.vueroutergwt.client.VueRouteComponent;
 import com.axellience.vueroutergwt.client.VueRouter;
+import com.google.gwt.user.client.Cookies;
+import elemental2.dom.Event;
 import jsinterop.annotations.JsMethod;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Component
-public class LoginComponent implements IsVueComponent ,VueRouteComponent,HasCreated {
+public class LoginComponent implements IsVueComponent, VueRouteComponent, HasCreated {
     Logger logger = Logger.getLogger(LoginComponent.class.getName());
     @Data
     Route route;
@@ -24,22 +27,55 @@ public class LoginComponent implements IsVueComponent ,VueRouteComponent,HasCrea
     String password;
     @Data
     boolean remember;
+    @Data
+    boolean active = false;
+
 
     @JsMethod
-    public void submit() {
-        logger.info("submit:"+email+","+password+","+remember);
-        if(!password.isEmpty()&&password.equals("000000")){
-            router.push("/Main");
-        }else{
-            //error
+    public void submit(Event event) {
+//        if (event != null) {
+//            event.preventDefault();
+//            logger.info(" -> Event Type: " + event.type);
+//        }
+        logger.info("submit:" + email + "," + password + "," + remember);
+        if (password == null || password.isEmpty()) {
+            return;
         }
+        if (password.equals("000000")) {
+            if(remember){
+                Date expires = new Date();
+                //设置cookie到期时间,从设置时间算起30天的毫秒之后失效
+                expires.setTime(expires.getTime() + 30*24*60*60*1000);
+                Cookies.setCookie("email", email, expires, null, "/", false);
+                Cookies.setCookie("password", password, expires, null, "/", false);
+                Cookies.setCookie("remember", remember?"0":"1", expires, null, "/", false);
+            }
+            router.push("/Main");
+        } else {
+            event.preventDefault();
+            //error
+            active = true;
+        }
+
     }
 
     @Override
     public void created() {
-        route =  getRoute();
+        logger.info("LoginComponent:created"  );
+        route = getRoute();
         router = getRouter();
-        logger.info("route:"+route);
-        logger.info("router:"+router);
+        String email = Cookies.getCookie("email");
+        String password = Cookies.getCookie("password");
+        String remember = Cookies.getCookie("remember");
+        logger.info("route:" + route);
+        logger.info("router:" + router);
+        logger.info("email:" + email);
+        logger.info("password:" + password);
+        logger.info("remember:" + remember);
+        this.email = email;
+        this.password = password;
+        if(remember!=null){
+            this.remember = remember.equals("0")?true:false;
+        }
     }
 }
