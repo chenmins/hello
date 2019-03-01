770b7366560e91457fc94d390d6dcc16a43e7ab9
Chenmin <cnlll@qq.com>
2019-03-01 18:12:37 +0800

    main mv some

--- src/main/java/org/chenmin/gwt/client/pages/main/TopBarComponent.java
+++ src/main/java/org/chenmin/gwt/client/pages/main/TopBarComponent.java
@@ -3,7 +3,7 @@ package org.chenmin.gwt.client.pages.main;
 import com.axellience.vuegwt.core.annotations.component.Component;
 import com.axellience.vuegwt.core.client.component.IsVueComponent;
 
-@Component(components = {TopBarComponent.class,SideBarComponent.class,WelcomeComponent.class})
-public class MainComponent implements IsVueComponent {
+@Component
+public class TopBarComponent implements IsVueComponent {
 
 }
