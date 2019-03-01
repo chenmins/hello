83016dac71e2f0b4609c21dfc1ec2fe71a255433
Chenmin <cnlll@qq.com>
2019-03-01 11:02:29 +0800

    Initial commit

---
+++ src/main/java/org/chenmin/gwt/client/Hello.java
@@ -0,0 +1,16 @@
+package org.chenmin.gwt.client;
+
+import com.axellience.vuegwt.core.client.Vue;
+import com.axellience.vuegwt.core.client.VueGWT;
+import com.google.gwt.core.client.EntryPoint;
+import org.chenmin.gwt.client.pages.TestBtnFactory;
+
+public class Hello implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        //    VueGWT.init();
+        VueGWT.initWithoutVueLib();
+        Vue.attach("#app",TestBtnFactory.get());
+//        Vue.attach("#app",TableFactory.get());
+    }
+}
