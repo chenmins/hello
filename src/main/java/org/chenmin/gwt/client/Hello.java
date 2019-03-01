d37446e11a69e704231c94ef96147226b2f47b28
Chenmin <cnlll@qq.com>
2019-03-01 13:54:23 +0800

    login main

--- src/main/java/org/chenmin/gwt/client/Hello.java
+++ src/main/java/org/chenmin/gwt/client/Hello.java
@@ -3,6 +3,8 @@ package org.chenmin.gwt.client;
 import com.axellience.vuegwt.core.client.Vue;
 import com.axellience.vuegwt.core.client.VueGWT;
 import com.google.gwt.core.client.EntryPoint;
+import org.chenmin.gwt.client.pages.LoginComponentFactory;
+import org.chenmin.gwt.client.pages.MainComponentFactory;
 import org.chenmin.gwt.client.pages.TestBtnFactory;
 
 public class Hello implements EntryPoint {
@@ -10,7 +12,8 @@ public class Hello implements EntryPoint {
     public void onModuleLoad() {
         //    VueGWT.init();
         VueGWT.initWithoutVueLib();
-        Vue.attach("#app",TestBtnFactory.get());
-//        Vue.attach("#app",TableFactory.get());
+//        Vue.attach("#app",TestBtnFactory.get());
+//        Vue.attach("#app",LoginComponentFactory.get());
+        Vue.attach("#app",MainComponentFactory.get());
     }
 }
---
+++ src/main/java/org/chenmin/gwt/client/pages/LoginComponent.java
@@ -0,0 +1,9 @@
+package org.chenmin.gwt.client.pages;
+
+import com.axellience.vuegwt.core.annotations.component.Component;
+import com.axellience.vuegwt.core.client.component.IsVueComponent;
+
+@Component
+public class LoginComponent implements IsVueComponent {
+
+}
