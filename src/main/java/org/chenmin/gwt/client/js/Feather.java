056aaf7e67708e1ef849df3145b8171f64c6d3d4
chenmin <admin@chenmin.org>
2019-03-02 11:11:43 +0800

    Feather init

---
+++ src/main/java/org/chenmin/gwt/client/js/Feather.java
@@ -0,0 +1,11 @@
+package org.chenmin.gwt.client.js;
+
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType(isNative=true,name="feather", namespace=JsPackage.GLOBAL)
+public class Feather {
+
+    public static native void replace();
+
+}
--- src/main/java/org/chenmin/gwt/client/pages/main/SideBarComponent.java
+++ src/main/java/org/chenmin/gwt/client/pages/main/SideBarComponent.java
@@ -2,8 +2,14 @@ package org.chenmin.gwt.client.pages.main;
 
 import com.axellience.vuegwt.core.annotations.component.Component;
 import com.axellience.vuegwt.core.client.component.IsVueComponent;
+import com.axellience.vuegwt.core.client.component.hooks.HasMounted;
+import org.chenmin.gwt.client.js.Feather;
 
 @Component
-public class SideBarComponent implements IsVueComponent {
+public class SideBarComponent implements IsVueComponent ,HasMounted {
 
+    @Override
+    public void mounted() {
+        Feather.replace();
+    }
 }
